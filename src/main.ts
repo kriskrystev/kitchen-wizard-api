30f4940dfd22755610ea517472a549c96551792e
kriskrystev <krystev.kristiyan@gmail.com>
2023-05-18 16:24:46 +0300

    Configure Swagger module

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,36 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
 import { PORT } from './constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
   app.enableCors({});
+
+  const config = new DocumentBuilder()
+    .setTitle('Recipes API')
+    .setDescription('This is a simple and straightforward recipes API')
+    .setVersion('1.0')
+    .addTag('users')
+    .addTag('recipes')
+    .addTag('ingredients')
+    .addTag('authentication')
+    .addBearerAuth({
+      type: 'http',
+      scheme: 'bearer',
+      bearerFormat: 'JWT',
+      name: 'JWT',
+      description: 'Enter JWT token',
+      in: 'header'
+    })
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(PORT);
 }
 
