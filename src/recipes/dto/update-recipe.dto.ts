017069c3c1e0902e78b8c29fe214bd66950c38a9
kriskrystev <krystev.kristiyan@gmail.com>
2023-05-19 19:09:40 +0300

    Change the imports on the update DTOs - this helps the swagger show update dto props

--- src/recipes/dto/update-recipe.dto.ts
+++ src/recipes/dto/update-recipe.dto.ts
@@ -1,4 +1,4 @@
-import { OmitType, PartialType } from '@nestjs/mapped-types';
+import { OmitType, PartialType } from '@nestjs/swagger';
 import { CreateRecipeDto } from './create-recipe.dto';
 
 export class UpdateRecipeDto extends PartialType(
