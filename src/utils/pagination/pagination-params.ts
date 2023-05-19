7bd946ef201c8e71d6d93630b9c3abd135fbd89a
kriskrystev <krystev.kristiyan@gmail.com>
2023-05-19 19:10:13 +0300

    Documenting PaginationParams properties

--- src/utils/pagination/pagination-params.ts
+++ src/utils/pagination/pagination-params.ts
@@ -1,17 +1,34 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import { IsMongoId, IsNumber, IsOptional, Min } from 'class-validator';
 
 export class PaginationParams {
+  @ApiProperty({
+    required: false,
+    description:
+      'MongoID from which to begin the search. This is used for a more optimized search, as it makes use of MongoDBs id nature'
+  })
   @IsOptional()
   @IsMongoId()
   startId?: string;
 
+  @ApiProperty({
+    required: false,
+    description: 'The amount of documents to skip.',
+    minimum: 0
+  })
   @IsOptional()
   @Type(() => Number)
   @IsNumber()
   @Min(0)
   skip?: number;
 
+  @ApiProperty({
+    required: false,
+    description:
+      'The maximum number of documents to return. If provided must be at least 1.',
+    minimum: 1
+  })
   @IsOptional()
   @Type(() => Number)
   @IsNumber()
