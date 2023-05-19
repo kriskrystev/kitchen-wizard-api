b43290fd9101feb0932cae5cc4e1d5971a07f78a
kriskrystev <krystev.kristiyan@gmail.com>
2023-05-19 19:09:52 +0300

    Add api property

--- src/ingredients/dto/read-ingredient.dto.ts
+++ src/ingredients/dto/read-ingredient.dto.ts
@@ -1,12 +1,16 @@
 import { AutoMap } from '@automapper/classes';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class ReadIngredientDto {
   @AutoMap()
+  @ApiProperty()
   id: string;
 
   @AutoMap()
+  @ApiProperty()
   title: string;
 
   @AutoMap()
+  @ApiProperty({ required: false })
   description: string;
 }
